1d6d41cf56de554805f03270b8406373fcbc5511
Seru <seru.style@gmail.com>
2019-03-28 17:52:27 +0900

    Continue implement theme change when view load

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     private lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairOfCards)
     private var currentTheme: Theme?
-    private var emojiChoices: String = ""
+    private var emojiChoices: String = "?"
     
     var previousCard: UIButton?
     
@@ -41,13 +41,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = currentTheme?.viewBG
+        updateViewFromModel()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         currentTheme = getRandomtheme()
-        emojiChoices = currentTheme?.icons ?? ""
+        emojiChoices = currentTheme?.icons ?? "?"
+
+        
         
     }
     
@@ -62,10 +67,10 @@ class ViewController: UIViewController {
     @IBAction private func newGameTapped(_ sender: UIButton) {
         game = Concentration(numberOfPairsOfCards: numberOfPairOfCards)
         currentTheme = getRandomtheme()
-        emojiChoices = currentTheme?.icons ?? ""
+        emojiChoices = currentTheme?.icons ?? "?"
         updateViewFromModel()
         updateFlipCountLabel()
-        self.view.backgroundColor = getRandomtheme().viewBG
+        self.view.backgroundColor = currentTheme?.viewBG
         
     }
     
@@ -93,7 +98,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
             } else {
                 button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0) : #colorLiteral(red: 1, green: 0.5745739937, blue: 0.001978197834, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0) : currentTheme?.cardBG
             }
             
         }
@@ -126,12 +131,12 @@ class ViewController: UIViewController {
     
     private func getRandomtheme() -> Theme {
         var themeArray: [Theme] = []
-        let themeHalloween = Theme(icons: "🎃👻🦇🙀😈👹💀👾🤡👁", cardBG: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), viewBG: #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1))
-        let themeAnimals = Theme(icons: "🐶🐻🐼🦢🕊🐯🦁🐮🐥🐤🐔🐝🦄🦀🦓🐎", cardBG: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), viewBG: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
-        let themeWeather = Theme(icons: "🌦🌤☀️🌈⛈❄️🌬☔️☂️☃️⛄️🌩🌧", cardBG: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), viewBG: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
-        let themeFood = Theme(icons: "🍎🍏🍌🍉🍒🍓🍆🥗🍜🍙🍰🍩", cardBG: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), viewBG: #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1))
-        let themeFaces = Theme(icons: "😆😘😍🧐😎😖😢🤓🤪😵", cardBG: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), viewBG: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
-        let themeTransportation = Theme(icons: "🚘🚗🚅✈️⛵️🚁🛸🚒🏎🚓🚑🚌🚕", cardBG: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), viewBG: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
+        let themeHalloween = Theme(icons: "🎃👻🦇🙀😈👹💀👾🤡👁", cardBG: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1), viewBG: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        let themeAnimals = Theme(icons: "🐶🐻🐼🦢🕊🐯🦁🐮🐥🐤🐔🐝🦄🦀🦓🐎", cardBG: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), viewBG: #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
+        let themeWeather = Theme(icons: "🌦🌤☀️🌈⛈❄️🌬☔️☂️☃️⛄️🌩🌧", cardBG: #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), viewBG: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+        let themeFood = Theme(icons: "🍎🍏🍌🍉🍒🍓🍆🥗🍜🍙🍰🍩", cardBG: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), viewBG: #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+        let themeFaces = Theme(icons: "😆😘😍🧐😎😖😢🤓🤪😵", cardBG: #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), viewBG: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
+        let themeTransportation = Theme(icons: "🚘🚗🚅✈️⛵️🚁🛸🚒🏎🚓🚑🚌🚕", cardBG: #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), viewBG: #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))
         
         themeArray += [themeHalloween, themeAnimals, themeWeather, themeFood, themeFaces, themeTransportation]
 

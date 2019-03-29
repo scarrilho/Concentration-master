7fbb5be702c4bb1adffcb1fcea79d105566992ba
sergio <seergio.carrilho@gmail.com>
2019-03-30 07:15:05 +0900

    Add score based on speed getting match

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -11,7 +11,8 @@ import Foundation
 struct Concentration {
     
     private(set) var cards = Array<Card>()
-    
+    var timeAtFirstTap: Date?
+ 
     private var cardsShown: [Int] = []
     private(set) var flipCount = 0
     
@@ -36,24 +37,54 @@ struct Concentration {
         if !cards[index].isMatched {
             flipCount += 1
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                
+
+                
                 if cards[index] == cards[matchIndex] {
                     cards[index].isMatched = true
                     cards[matchIndex].isMatched = true
                     score += 2
+                    
+                    //Capture time for second tap and calculate score for matching cards: let elapsed = Date().timeIntervalSince(timeAtPress)
+                    if timeAtFirstTap != nil {
+                        let elapsed = Double(Date().timeIntervalSince(timeAtFirstTap!))
+                        
+                        if (elapsed < 0.5) {
+                            score += 1
+                            //print("Quick Move = \(elapsed)")
+                        } else {
+                            //do nothing
+                            //print("Slow Move = \(elapsed)")
+                        }
+                        
+                    }
+                    
                 } else {//Cards didn't match
                     if isIndexInCardShownArray(cardIndex: matchIndex) {score -= 1}
                     if isIndexInCardShownArray(cardIndex: index) {score -= 1}
-                    
                 }
                 cards[index].isFaceUp = true
             } else {
                 //Either no cards face up or two cards face up
                 indexOfOneAndOnlyFaceUpCard = index
+                //Capture time of first tap here
+                timeAtFirstTap = Date()
             }
         }
         
         //print("Score : \(score)")
         
+    }
+    
+    //Return points based on the speed to match two cards
+    private func scorePointsByTime(elapsed: Double) -> Double {
+        if (elapsed < 0.5) {
+            return 1
+        } else if (elapsed < 1){
+            return 0.5
+        } else {
+        return 0
+        }
     }
     
     private mutating func isIndexInCardShownArray(cardIndex: Int) -> Bool {
